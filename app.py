fa660e093923d4fa04031f3418afbad79cd1e2d9
IDGS-802-23002412 <rdannabuendia@gmail.com>
2026-02-25 20:26:52 -0600

    ABC completo 25/02/2026

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import flash
 from flask_wtf.csrf import CSRFProtect 
 from config import DevelopmentConfig 
 from flask import g 
+from flask_migrate import Migrate
 
 import forms 
 from models import db, Alumnos
@@ -10,7 +11,9 @@ from models import db, Alumnos
 app = Flask(__name__) 
 app.config.from_object(DevelopmentConfig) 
 db.init_app(app) 
-csrf = CSRFProtect() 
+csrf = CSRFProtect()
+migrate = Migrate(app,db)
+
 
 @app.errorhandler(404) 
 def page_not_found(e): 
@@ -52,15 +55,16 @@ def detalles():
 @app.route("/modificar", methods=['GET', 'POST'])
 def modificar():
     create_form = forms.UserForm2(request.form)
-    id = request.args.get('id') or request.form.get('id')
-    alum1 = db.session.query(Alumnos).filter(Alumnos.id == id).first()
-
     if request.method == 'GET':
+        id = request.args.get('id')
+        alum1 = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+        create_form.id.data = request.args.get('id')
         create_form.nombre.data = alum1.nombre
         create_form.apaterno.data = alum1.apaterno
         create_form.email.data = alum1.email
 
     if request.method == 'POST':
+        id = create_form.id.data
         alum1.nombre = create_form.nombre.data.strip()
         alum1.apaterno = create_form.apaterno.data
         alum1.email = create_form.email.data
@@ -69,7 +73,27 @@ def modificar():
         db.session.commit()
         return redirect(url_for('index'))
 
-    return render_template("modificar.html", form=create_form, id=id)
+    return render_template("modificar.html", form=create_form)
+
+@app.route("/eliminar", methods=['GET', 'POST'])
+def eliminar():
+    create_form = forms.UserForm2(request.form)
+    if request.method == 'GET':
+        id = request.args.get('id')
+        alum1 = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+        create_form.id.data = request.args.get('id')    
+        create_form.nombre.data = alum1.nombre
+        create_form.apaterno.data = alum1.apaterno
+        create_form.email.data = alum1.email
+    if request.method == 'POST':
+        id = create_form.id.data
+        alum1 = Alumnos.query.get(id)
+        
+        db.session.add(alum1)
+        db.session.commit()
+        return redirect(url_for('index'))
+
+    return render_template("eliminar.html", form=create_form)
 
 if __name__ == '__main__': 
     csrf.init_app(app) 
